fbb92acfbe9b1e14c82d90e3acf5080dbf442210
PDS Sandeep <sandeep.pds@cloudlex.com>
2023-12-13 23:47:47 +0530

    child to parent data passing

--- src/app/Runningnotes/inputdecorator/inputdecorator.component.ts
+++ src/app/Runningnotes/inputdecorator/inputdecorator.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-inputdecorator',
@@ -7,4 +7,8 @@ import { Component, Input } from '@angular/core';
 })
 export class InputdecoratorComponent {
   @Input() public fromParent: any;
+  @Output() public sendingMessageFromChild = new EventEmitter();
+  childtoParent(){
+    this.sendingMessageFromChild.emit("On Click of the button Data received from child to parent")
+  }
 }
--- src/app/Runningnotes/outputdecorator/outputdecorator.component.ts
+++ src/app/Runningnotes/outputdecorator/outputdecorator.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class OutputdecoratorComponent {
   public parentdata = "This data is coming from parent using @Input Decorator"
+  public childtoparent : any;
 }
